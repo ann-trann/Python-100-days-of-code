ff96a75a1eccd74ba6188e2aec371dd128a4d04d
Trần Thúy An <122709489+ann-trann@users.noreply.github.com>
2023-11-05 14:24:46 +0700

    Add files via upload

---
+++ Day 17 - The Quiz Project/data.py
@@ -0,0 +1,16 @@
+question_data = [
+    {"text": "A slug's is green.", "answer": "True"},
+    {"text": "The loudest animal is the African Elephant.", "answer": "True"},
+    {"text": "Approximately one quarter of human bones are in the feet.", "answer": "True"},
+    {"text": "The total surface area of a human ling us the size of a football pitch.", "answer": "True"},
+    {"text": "In West Virgina, USA, if you accidentally hit an animal with your car, "
+             "you are free to tak it home to eat.", "answer": "True"},
+    {"text": "In London, UK, if you happen to die in the House of Parliament, "
+             "you are entitled to a state funeral.", "answer": "False"},
+    {"text": "It is illegal to pee in the Ocean in Portugal.", "answer": "True"},
+    {"text": "You can lead a cow down stairs but not upstairs.", "answer": "False"},
+    {"text": "Google was originally called 'Backrub'.", "answer": "True"},
+    {"text": "Buzz Aldrin's mother's maiden name was 'Moon'.", "answer": "True"},
+    {"text": "No piece of square dry paper can be folded in half more than 7 times.", "answer": "False"},
+    {"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"}
+]
