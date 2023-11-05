fccc83ee50c411571980a36b71e1daf27a2bc6f5
Trần Thúy An <122709489+ann-trann@users.noreply.github.com>
2023-11-05 16:28:59 +0700

    Update and rename Turtle Random Walk to Turtle Random Walk.py

--- Day 18 - Turtle & GUI/Turtle Random Walk.py
+++ Day 18 - Turtle & GUI/Turtle Random Walk.py
@@ -1,4 +1,4 @@
-########################################## My code #########################################################################
+########################################## My code ##########################################
 from turtle import Turtle, Screen
 import random
 
@@ -6,7 +6,7 @@ tim = Turtle()
 tim.shape("turtle")
 
 colors = ["green", "blue", "red", "pink", "brown", "black", "grey", "purple"]
-direction = [0, 1]
+direction = [0, 1, 2]
 
 tim.speed(10)
 tim.width(10)
@@ -15,7 +15,9 @@ for i in range(200):
     tim.forward(20)
     ran = random.choice(direction)
     tim.color(random.choice(colors))
-    if ran:
+    if ran == 0:
+        tim.forward(20)
+    elif ran == 1: 
         tim.right(90)
     else:
         tim.left(90)
